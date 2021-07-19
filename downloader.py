d11cd59211784c3a92f082ae32b3853104f40571
LitingLin <linliting06@live.com>
2021-07-19 16:16:40 +0800

    drop state persistence support

--- downloader.py
+++ downloader.py
@@ -6,9 +6,6 @@ import time
 import enum
 import tqdm
 import numpy as np
-from contextlib import closing
-import lmdb
-import pickle
 import traceback
 using_undetected_chrome_driver = True
 if using_undetected_chrome_driver:
@@ -26,6 +23,9 @@ class DownloaderState(enum.Enum):
     Fail = enum.auto()
 
 
+_image_file_extensions = ('.jpg', '.jpeg', '.gif', '.webp', '.png')
+
+
 def _download_from_requests(requests, save_path, downloaded_images: set, target_number):
     valid_count = 0
     for request in requests:
@@ -37,7 +37,7 @@ def _download_from_requests(requests, save_path, downloaded_images: set, target_
         if content_type is None:
             continue
         ext = guess_extension(content_type.split(';')[0].strip())
-        if ext is None or ext not in ('.jpg', '.jpeg', '.gif', '.webp', '.png'):
+        if ext is None or ext not in _image_file_extensions:
             continue
 
         if request.response.body is None:
@@ -46,8 +46,10 @@ def _download_from_requests(requests, save_path, downloaded_images: set, target_
         file_name = request.url.split('/')[-1]
         if file_name in downloaded_images:
             continue
-        with open(os.path.join(save_path, file_name), 'wb') as f:
+        image_file_path = os.path.join(save_path, file_name)
+        with open(image_file_path + '.tmp', 'wb') as f:
             f.write(request.response.body)
+        os.rename(image_file_path + '.tmp', image_file_path)
         print(f'{len(downloaded_images)}/{target_number}, {file_name}')
         downloaded_images.add(file_name)
         if len(downloaded_images) >= target_number:
@@ -87,81 +89,74 @@ def _download_loop(driver: webdriver.Chrome, save_path: str, downloaded_images: 
             return True
 
 
-def _download(state_persistent_file: str, keyword: str, search_name: str, save_path: str, target_number: int,
-              proxy_address: str, headless: bool):
+def _download(search_name: str, save_path: str, target_number: int, proxy_address: str, headless: bool):
     rng = np.random.Generator(np.random.PCG64())
-    with closing(lmdb.open(state_persistent_file)) as persistent_storage:
-        with persistent_storage.begin(write=False, buffers=True) as txn:
-            downloaded_images = txn.get(keyword.encode('utf-8'))
-        if downloaded_images is None:
-            downloaded_images = set()
-        else:
-            downloaded_images = pickle.loads(downloaded_images)
-        if len(downloaded_images) >= target_number:
-            return DownloaderState.Ok
-        if not os.path.exists(save_path):
-            os.mkdir(save_path)
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+        downloaded_images = set()
+    else:
+        images = os.listdir(save_path)
+        images = [image for image in images if image.endswith(_image_file_extensions)]
+        downloaded_images = set(images)
 
-        webdriver_options = {}
+    if len(downloaded_images) >= target_number:
+        return DownloaderState.Ok
 
-        if headless:
-            chrome_options = webdriver.ChromeOptions()
-            chrome_options.add_argument('--headless')
-            webdriver_options['options'] = chrome_options
+    webdriver_options = {}
 
-        if proxy_address is not None:
-            webdriver_options['seleniumwire_options'] = {
-                'proxy': {
-                    'http': proxy_address,
-                    'https': proxy_address,
-                    'no_proxy': 'localhost,127.0.0.1'  # excludes
-                }
+    if headless:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        webdriver_options['options'] = chrome_options
+
+    if proxy_address is not None:
+        webdriver_options['seleniumwire_options'] = {
+            'proxy': {
+                'http': proxy_address,
+                'https': proxy_address,
+                'no_proxy': 'localhost,127.0.0.1'  # excludes
             }
-        fault_tolerance = 2
-        tried_times = 0
-        while True:
-            if tried_times == fault_tolerance:
+        }
+    fault_tolerance = 2
+    tried_times = 0
+    while True:
+        if tried_times == fault_tolerance:
+            break
+        try:
+            driver = webdriver.Chrome(os.path.join(os.path.dirname(__file__), 'drivers/chromedriver'), **webdriver_options)
+            with driver:
+                driver.get(f'https://id.pinterest.com/search/pins/?q={urllib.parse.quote(search_name)}&rs=typed')
+                success_flag = _download_loop(driver, save_path, downloaded_images, target_number, rng)
                 break
-            try:
-                driver = webdriver.Chrome(os.path.join(os.path.dirname(__file__), 'drivers/chromedriver'), **webdriver_options)
-                with driver:
-                    driver.get(f'https://id.pinterest.com/search/pins/?q={urllib.parse.quote(search_name)}&rs=typed')
-                    success_flag = _download_loop(driver, save_path, downloaded_images, target_number, rng)
-                    break
-            except Exception:
-                print(traceback.format_exc())
-                tried_times += 1
-        if len(downloaded_images) > 0:
-            with persistent_storage.begin(write=True, buffers=False) as txn:
-                txn.put(keyword.encode('utf-8'), pickle.dumps(downloaded_images))
+        except Exception:
+            print(traceback.format_exc())
+            tried_times += 1
 
-        if success_flag:
-            if len(downloaded_images) < target_number:
-                return DownloaderState.Partly
-            else:
-                return DownloaderState.Ok
+    if success_flag:
+        if len(downloaded_images) < target_number:
+            return DownloaderState.Partly
         else:
-            return DownloaderState.Fail
+            return DownloaderState.Ok
+    else:
+        return DownloaderState.Fail
 
 
-def download_worker_entry(state_persistent_file, keyword, search_name, save_path, target_number, proxy_address, headless,
+def download_worker_entry(search_name, save_path, target_number, proxy_address, headless,
                           shared_value: multiprocessing.Value):
-    state = _download(state_persistent_file, keyword, search_name, save_path, target_number, proxy_address, headless)
+    state = _download(search_name, save_path, target_number, proxy_address, headless)
     shared_value.value = state.value
 
 
 class PInterestDownloader:
-    def __init__(self, state_persistent_file: str, enable_multiprocessing=True, proxy_address=None, headless=False):
-        self.state_persistent_file = state_persistent_file
+    def __init__(self, enable_multiprocessing=True, proxy_address=None, headless=False):
         self.subprocess_state_value = multiprocessing.Value('i') if enable_multiprocessing else None
         self.proxy_address = proxy_address
         self.headless = headless
 
-    def download(self, keyword, search_name, save_path, target_number):
+    def download(self, search_name, save_path, target_number):
         if self.subprocess_state_value:
             p = multiprocessing.Process(target=download_worker_entry,
-                                        args=(self.state_persistent_file, keyword, search_name, save_path,
-                                              target_number, self.proxy_address, self.headless,
+                                        args=(search_name, save_path, target_number, self.proxy_address, self.headless,
                                               self.subprocess_state_value))
             p.start()
             p.join()
@@ -169,8 +164,7 @@ class PInterestDownloader:
                 return DownloaderState(DownloaderState.Fail)
             return DownloaderState(self.subprocess_state_value.value)
         else:
-            return _download(self.state_persistent_file, keyword, search_name, save_path, target_number,
-                             self.proxy_address, self.headless)
+            return _download(search_name, save_path, target_number, self.proxy_address, self.headless)
 
 
 def load_wordnet_ids(file_path: str):
@@ -206,8 +200,7 @@ def download(target_number, target_path, enable_multiprocessing, proxy_address, 
     wordnet_ids = load_wordnet_ids(os.path.join(os.path.dirname(__file__), 'imagenet21k_wordnet_ids.txt'))
     wordnet_lemmas = load_wordnet_lemmas(os.path.join(os.path.dirname(__file__), 'imagenet21k_wordnet_lemmas.txt'))
     assert len(wordnet_ids) == len(wordnet_lemmas)
-    downloader = PInterestDownloader(os.path.join(target_path, 'downloader_state'),
-                                     enable_multiprocessing,
+    downloader = PInterestDownloader(enable_multiprocessing,
                                      proxy_address,
                                      headless)
 
@@ -217,7 +210,7 @@ def download(target_number, target_path, enable_multiprocessing, proxy_address, 
     with tqdm.tqdm(total=len(wordnet_ids), ) as process_bar:
         for wordnet_id, wordnet_lemma in tqdm.tqdm(zip(wordnet_ids, wordnet_lemmas)):
             process_bar.set_description(f'{wordnet_id}: {wordnet_lemma}')
-            downloader_state = downloader.download(wordnet_id, wordnet_lemma,
+            downloader_state = downloader.download(wordnet_lemma,
                                                    os.path.join(target_path, wordnet_id), target_number)
             if downloader_state != DownloaderState.Fail:
                 fail_times = 0
